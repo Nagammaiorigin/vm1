6e1d4627d4bd136d09fb513f51a1c82f0fb9a25e
nagu <nagammai.origin@gmail.com>
2024-06-01 14:03:24 -0400

    removed main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 # Resource Group
 resource "azurerm_resource_group" "example" {
   name     = "myResourceGroup"
-  location = "UK South"
+  location = "east US 2"
 }
 
 # Virtual Network
